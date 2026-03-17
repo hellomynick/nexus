e2763fa4a64506131e2a82ab6eecbf5fb01d71f0
Vu Tran <vu.tran@dmtholding.com>
2026-03-17 17:47:11 +0700

    hls feature

--- src/hls/m3u8.rs
+++ src/hls/m3u8.rs
@@ -1,7 +1,4 @@
-use std::{
-    str::Bytes,
-    sync::{LazyLock, RwLock},
-};
+use std::sync::{LazyLock, RwLock};
 
 pub const M3U_TAG: &str = "#EXTM3U";
 pub const VER_TAG_V3: &str = "#EXT-X-VERSION:3";
@@ -12,20 +9,23 @@ pub const SEGMENT_TAG: &str = "#EXT-X-INDEPENDENT-SEGMENTS";
 pub const CHUNK: &str = "Chunk_";
 pub const INF_TAG: &str = "#EXTINF:";
 pub const URI_TAG: &str = "URI";
+
 pub const DOWN_LINE_SYMBOL: &str = "\n";
+pub const COMMA: &str = ",";
+pub const TS_EXT: &str = ".ts";
 
 pub static GLOBAL_M3U8_BUFFER: LazyLock<RwLock<String>> =
     LazyLock::new(|| RwLock::new(String::with_capacity(1024)));
 
-fn generate_hls_response_m3u8_v3(duration: u8, seq: u32, chunk_duration: f32, chunk_id: u32) {
+pub fn generate_hls_response_m3u8_v3(duration: u8, seq: u32, chunk_duration: f32, chunk_id: u32) {
     let mut num_buf = itoa::Buffer::new();
     let mut float_buf = ryu::Buffer::new();
 
     let mut buffer = match GLOBAL_M3U8_BUFFER.write() {
         Ok(lock) => lock,
-        Err(poison_err) => {
-            println!("Failed to acquire write lock: {poison_err:?}");
-            poison_err.into_inner()
+        Err(poisoned) => {
+            println!("Failed to acquire write lock: {poisoned:?}");
+            poisoned.into_inner()
         }
     };
 
@@ -56,9 +56,23 @@ fn generate_hls_response_m3u8_v3(duration: u8, seq: u32, chunk_duration: f32, ch
     // Inf
     buffer.push_str(INF_TAG);
     buffer.push_str(float_buf.format(chunk_duration));
+    buffer.push_str(COMMA);
     buffer.push_str(DOWN_LINE_SYMBOL);
 
     // Chunk video
     buffer.push_str(CHUNK);
-    buffer.push_str(num_buf.format(chunk_id))
+    buffer.push_str(num_buf.format(chunk_id));
+    buffer.push_str(TS_EXT);
+    buffer.push_str(DOWN_LINE_SYMBOL);
+}
+
+pub fn read_m3u8() -> Vec<u8> {
+    let buffer = match GLOBAL_M3U8_BUFFER.read() {
+        Ok(lock) => lock,
+        Err(poisoned) => {
+            panic!("Failed to acquire read lock: {poisoned:?}");
+        }
+    };
+
+    return buffer.as_bytes().to_vec();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
-pub mod hls;
+use axum::{Json, Router, http::StatusCode, response::IntoResponse, routing::get};
+
 pub mod common;
+mod hls;
 pub mod server_config;
 
-fn main() {}
+const PORT: &str = "9320";
+
+#[tokio::main]
+async fn main() {
+    let text = format!("{:?}", String::from_utf8(data).unwrap());
+    print!("{}", text);
+
+    let app = Router::new().route("/m3u8", get(build_m3u8));
+
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{PORT}"))
+        .await
+        .expect("Can't run app on port");
+}
+
+fn build_m3u8() -> IntoResponse {
+    let _ = hls::m3u8::generate_hls_response_m3u8_v3(3, 1, 3.0, 1);
+    let data = hls::m3u8::read_m3u8();
+}
